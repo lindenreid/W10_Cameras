078762d322d628e6832ceb73ee842e6d58e0c900
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2025-03-10 18:40:21 -0700

    added splitscreen demo

--- Assets/Scripts/Player2.cs
+++ Assets/Scripts/Player2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player2 : MonoBehaviour
 {
     [SerializeField] private float _forwardSpeed = 1.0f;
     [SerializeField] private float _turnSpeed = 1.0f;
@@ -10,8 +10,8 @@ public class Player : MonoBehaviour
     // movement
     private void Update ()
     {
-        float translation = Input.GetAxis("P1_Vertical") * _forwardSpeed * Time.deltaTime;
-        float rotation = Input.GetAxis("P1_Horizontal") * _turnSpeed * Time.deltaTime;
+        float translation = Input.GetAxis("P2_Vertical") * _forwardSpeed * Time.deltaTime;
+        float rotation = Input.GetAxis("P2_Horizontal") * _turnSpeed * Time.deltaTime;
 
         transform.Translate(0, 0, translation);
         transform.Rotate(0, rotation, 0);
